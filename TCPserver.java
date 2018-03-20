6c526859b860fca2d8ceaa2b721e4588c296fb37
Mini <minicthomas921@gmail.com>
2018-03-20 15:15:24 +0530

    renamed server code

--- TCPserver.java
+++ TCPserver.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.net.*;
 
-class TCPServer {
+class TCPserver {
  public static void main(String argv[]) throws Exception {
   String clientSentence;
   String capitalizedSentence;
