953c8a4a17895c016aa9a9e24b76f3491e9334b4
Mini <minicthomas921@gmail.com>
2018-03-20 14:54:38 +0530

    client code Completed

--- client.java
+++ client.java
@@ -10,5 +10,10 @@ BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in))
   DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
   BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
   sentence = inFromUser.readLine(); 
+ outToServer.writeBytes(sentence + '\n');
+  modifiedSentence = inFromServer.readLine();
+  System.out.println("FROM SERVER: " + modifiedSentence);
+  clientSocket.close();
+ 
 }
   }
