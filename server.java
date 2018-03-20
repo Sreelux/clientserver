3c353672f65bac4746428db5d61020c3c1da7fc4
Mini <minicthomas921@gmail.com>
2018-03-20 15:06:03 +0530

    server code completed

--- server.java
+++ server.java
@@ -7,3 +7,15 @@ class TCPServer {
   String capitalizedSentence;
   ServerSocket welcomeSocket = new ServerSocket(6789);
 
+  while (true) {
+   Socket connectionSocket = welcomeSocket.accept();
+   BufferedReader inFromClient =
+    new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+   DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+   clientSentence = inFromClient.readLine();
+   System.out.println("Received: " + clientSentence);
+   capitalizedSentence = clientSentence.toUpperCase() + '\n';
+   outToClient.writeBytes(capitalizedSentence);
+  }
+}
+}
