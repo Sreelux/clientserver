81ea08d857bb3f3435f037c038e8310747a6763d
Mini <minicthomas921@gmail.com>
2018-03-20 14:47:15 +0530

    Output stream and buffereader object added

--- client.java
+++ client.java
@@ -6,6 +6,9 @@ class TCPClient {
   String sentence;
   String modifiedSentence;
 BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-  
+  Socket clientSocket = new Socket("localhost", 6789);
+  DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+  BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+  sentence = inFromUser.readLine(); 
 }
   }
